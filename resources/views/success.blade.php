3f2024d95557456c83f7f103b7d66f1cd71eaf37
yaelahtam <rezaaditya.131202@gmail.com>
2020-07-14 07:55:51 +0700

    aduan

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,23 +10,24 @@
     <link href="https://fonts.googleapis.com/css?family=Assistant:200,300,400,600,700,800|Playfair+Display:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="frontend/styles/main.css">
 </head>
+
 <body>
 
     <!--Navbar-->
     <div class="container">
         <nav class="row navbar navbar-expand-lg navbar-light bg-white">
-           <div class="navbar-nav ml-auto mr-auto mr-sm-auto mr-lg-0 mr-md-auto">
-               <a href="index.html" class="navbar-brand">
-                   LOGO
-               </a>
-           </div>
-           <ul class="navbar-nav mr-auto d-none d-lg-block">
-               <li>
-                   <span class="text-muted">
-                    | &nbsp; AduanSiswa SMK Jakarta Pusat 1
-                   </span>
-               </li>
-           </ul>
+            <div class="navbar-nav ml-auto mr-auto mr-sm-auto mr-lg-0 mr-md-auto">
+                <a href="index.html" class="navbar-brand">
+                    LOGO
+                </a>
+            </div>
+            <ul class="navbar-nav mr-auto d-none d-lg-block">
+                <li>
+                    <span class="text-muted">
+                        | &nbsp; AduanSiswa SMK Jakarta Pusat 1
+                    </span>
+                </li>
+            </ul>
         </nav>
     </div>
 
@@ -56,6 +58,7 @@
     <script src="frontend/libraries/jquery/jquery-3.4.1.min.js"></script>
     <script src="frontend/libraries/bootstrap/js/bootstrap.js"></script>
     <script src="frontend/libraries/retina/retina.min.js"></script>
-   
+
 </body>
+
 </html
